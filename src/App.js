8fcb330547a0b4504fdd3237b848b99e5bd2079b
ushasree10 <ushasreereddy1011@gmail.com>
2023-10-12 11:31:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { Paragraph } from './components/Paragraph';
+
+function App() {
+  return (
+    <div>
+      <h2 style={{color:"black"}}>React-Task-1</h2>
+      <Paragraph />
+    </div>
+  );
+}
+
+export default App;
